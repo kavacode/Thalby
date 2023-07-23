de7d936b1e7480666e83cbed61b373f8139e46d2
Serhii Kaunenko <121514834+kavacode@users.noreply.github.com>
2023-07-23 20:19:14 +0300

    Add files via upload
    finish 1.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,39 @@
-var menuBtn = document.querySelector('.menu-btn');
-var nav = document.querySelector('nav');
-var lineOne = document.querySelector('nav .menu-btn .line--1');
-var lineTwo = document.querySelector('nav .menu-btn .line--2');
-var lineThree = document.querySelector('nav .menu-btn .line--3');
-var link = document.querySelector('nav .nav-links');
-menuBtn.addEventListener('click', () => {
-    nav.classList.toggle('nav-open');
-    lineOne.classList.toggle('line-cross');
-    lineTwo.classList.toggle('line-fade-out');
-    lineThree.classList.toggle('line-cross');
-    link.classList.toggle('fade-in');
-})
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        // animateOut: 'slideOutUp',
+        // animateIn: 'slideInUp',
+        navText: ['', ' '],
+        nav: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1,
+                center: true
+            },
+            674: {
+                items: 2
+
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+});
+// меню бургер
+    const btmMenu = document.querySelector('.menu__btm');
+    if (btmMenu) {
+        const menuBody = document.querySelector('.menu__body');
+        const menuIcon = document.querySelector('.menu__icon');
+        btmMenu.addEventListener("click", function (e) {
+            document.body.classList.toggle('_lock');
+            btmMenu.classList.toggle('_active');
+            menuBody.classList.toggle('_active');
+            menuIcon.classList.toggle('_active');
+        });
+    }
+
+
